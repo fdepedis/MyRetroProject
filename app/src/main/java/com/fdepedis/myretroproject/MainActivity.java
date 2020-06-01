5899d93a0d4d7afcde7a445eba7072efbfa78d15
Flax <flavio.depedis@gmail.com>
2020-06-01 16:10:53 +0200

    Add fix

--- app/src/main/java/com/fdepedis/myretroproject/MainActivity.java
+++ app/src/main/java/com/fdepedis/myretroproject/MainActivity.java
@@ -20,6 +20,7 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
+
     private PhotoAdapter adapter;
     private RecyclerView recyclerView;
     ProgressDialog progressDoalog;
@@ -59,4 +60,5 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setAdapter(adapter);
     }
+
 }
